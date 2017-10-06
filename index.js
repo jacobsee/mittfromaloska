a31ca6df177abd06907ea51889b1aeda2308660f
Jacob See <jacob.d.see@gmail.com>
2017-10-06 00:23:48 -0800

    re-add scrambling

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ var T = new Twit({
   timeout_ms: 60 * 1000
 });
 
+var replacements = [
+  {
+    find: ["a", "e", "i", "o", "u"],
+    replace: ["a", "e", "i", "o", "u"],
+    probability: 0.2
+  },
+  {
+    find: ["A", "E", "I", "O", "U"],
+    replace: ["A", "E", "I", "O", "U"],
+    probability: 0.2
+  }
+];
+
 String.prototype.replaceAt = function(index, replacement) {
   return (
     this.substr(0, index) +
@@ -35,6 +48,16 @@ stream.on("tweet", function(tweet, err) {
   words.forEach(word => {
     if (!word.includes("@") && !word.includes("http")) {
       // let's not mention people or URLs...
+      for (var i = 0, len = word.length; i < len; i++) {
+        replacements.forEach(replacement => {
+          if (
+            replacement.find.includes(word[i]) &&
+            Math.random() < replacement.probability
+          ) {
+            word = word.replaceAt(i, _.sample(replacement.replace));
+          }
+        });
+      }
       outputWords.push(word);
     }
   });
